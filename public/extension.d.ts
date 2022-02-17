f011517fd70228ce596d54cfd1fd123127df40bd
Tom <tavedissian@k2geospatial.com>
2022-02-17 10:15:36 -0500

    JWNG-403 - Add coma

--- public/extension.d.ts
+++ public/extension.d.ts
@@ -39,7 +39,7 @@ declare interface JAppExtension extends JCoreExtension {
   /**
    * The panel icon.
    * 
-   * An url (png, jpeg, svg, etc ...), a base 64 string or an svg tag as a string as you like.
+   * An url (png, jpeg, svg, etc ...), a base 64 string or an svg tag as a string, as you like.
    * For an svg string a viewbox need to be specified for the icon to appear,
    * if no fill attribute is specified, the icon will follow the app theme.
    */

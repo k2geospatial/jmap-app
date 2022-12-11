ef8527f22b372f72c1b307334c58b00ecd4f4df3
Jean Kiepura <jkiepura@k2geospatial.com>
2022-12-11 08:08:51 -0500

    [bugfix/JWNG-735] - typo

--- public/app.d.ts
+++ public/app.d.ts
@@ -960,7 +960,7 @@ declare namespace JMap {
        * 
        * Default measurement system is "geodetic", meaning that all measures are representative of the length or area of features on the surface of the Earth. This is what is normally desired.
        * 
-       * In some rare situations, someone may want to get the length or area of drawn feature in the map's projection plan (EPSG:3857 - WGS 84 / Pseudo-Mercator). in this case, they can set the measurement system to "planar". 
+       * In some rare situations, someone may want to get the length or area of drawn features in the map's projection plan (EPSG:3857 - WGS 84 / Pseudo-Mercator). in this case, they can set the measurement system to "planar". 
        * 
        * Be warned that lengths or areas in planar context are not representative of the real dimensions on the earth's surface. The only supported planar projection for measures is EPSG:3857.
        * 
